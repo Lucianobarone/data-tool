663e0c489db183b639d3adbb00a03639a92cf5fa
Lucianobarone <luciano.barone@hotmail.com>
2020-12-14 10:24:04 -0300

    app data-tool completed

--- data-tool-api/src/config/db/index.js
+++ data-tool-api/src/config/db/index.js
@@ -1,8 +1,11 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 const config = require('../env/env.json');
 
-const db = new Sequelize(`postgres://postgres:argentina09@localhost:5432/${config.DATABASE}`, {
-    logging: false
-});
+const db = new Sequelize(
+  `postgres://${config.USER}${config.PASSWORD}@localhost:5432/${config.DATABASE}`,
+  {
+    logging: false,
+  },
+);
 
-module.exports = db
+module.exports = db;
